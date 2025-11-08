df7eebb945ddfb959e6495ce22a78d77d85a12d6
Özer <ozeruzunkaya12@gmail.com>
2025-11-08 22:53:35 +0300

    automapper added

---
+++ ECommerce.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using ECommerce.Application.DTOs;
+using ECommerce.Domain.Entities;
+
+namespace ECommerce.Application.Mapping;
+
+public sealed class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Product, ProductDto>()
+            .ForCtorParam("Id", o => o.MapFrom(s => s.Id))
+            .ForCtorParam("Name", o => o.MapFrom(s => s.Name))
+            .ForCtorParam("Amount", o => o.MapFrom(s => s.Price.Amount))
+            .ForCtorParam("Currency", o => o.MapFrom(s => s.Price.Currency));
+
+        CreateMap<Order, OrderDto>()
+            .ForCtorParam("Id", o => o.MapFrom(s => s.Id))
+            .ForCtorParam("Status", o => o.MapFrom(s => s.Status))
+            .ForCtorParam("CreatedAt", o => o.MapFrom(s => s.CreatedAt))
+            .ForCtorParam("TotalAmount", o => o.MapFrom(s => s.Items.Sum(i => i.UnitPrice.Amount * i.Quantity)))
+            .ForCtorParam("Currency", o => o.MapFrom(s => s.Items.First().UnitPrice.Currency));
+    }
+}
