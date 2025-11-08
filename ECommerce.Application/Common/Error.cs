512b51adcdd373b1e04b6bafd77b6794fdeb9a7d
Özer <ozeruzunkaya12@gmail.com>
2025-11-08 22:56:46 +0300

    error fix

--- ECommerce.Application/Common/Error.cs
+++ ECommerce.Application/Common/Error.cs
@@ -1,8 +1,8 @@
 ﻿namespace ECommerce.Application.Common;
 
-public sealed record Error(string Code, string Message)
+public sealed record Error(int Code, string Message)
 {
-    public static readonly Error Validation = new("validation_error", "Doğrulama hatası.");
-    public static readonly Error NotFound = new("not_found", "Kayıt bulunamadı.");
-    public static Error External(string code, string message) => new(code, message);
+    public static readonly Error Validation = new(-1, "Doğrulama hatası.");
+    public static readonly Error NotFound = new(-99, "Kayıt bulunamadı.");
+    public static Error External(int code, string message) => new(code, message);
 }
