83d24b2b8abaae92a2211007fde787d4202312de
Özer <ozeruzunkaya12@gmail.com>
2025-11-08 22:59:31 +0300

    product query added

---
+++ ECommerce.Application/Products/Queries/GetProductsQuery.cs
@@ -0,0 +1,7 @@
+﻿using ECommerce.Application.Common;
+using ECommerce.Application.DTOs;
+using MediatR;
+
+namespace ECommerce.Application.Products.Queries;
+
+public sealed record GetProductsQuery() : IRequest<Result<IReadOnlyList<ProductDto>>>;
---
+++ ECommerce.Application/Products/Queries/GetProductsQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using ECommerce.Application.Abstractions;
+using ECommerce.Application.Common;
+using ECommerce.Application.DTOs;
+using MediatR;
+
+namespace ECommerce.Application.Products.Queries;
+
+public sealed class GetProductsQueryHandler(IBalanceManagementClient balanceClient) : IRequestHandler<GetProductsQuery, Result<IReadOnlyList<ProductDto>>>
+{
+    public async Task<Result<IReadOnlyList<ProductDto>>> Handle(GetProductsQuery request, CancellationToken ct)
+    {
+        // Dış servisten ürünleri al
+        var resp = await balanceClient.GetProductsAsync(ct);
+
+        var result = resp.Products
+            .Select(p => new ProductDto(Guid.Parse(p.Id), p.Name, p.Price, p.Currency))
+            .ToList()
+            .AsReadOnly();
+
+        return Result<IReadOnlyList<ProductDto>>.Success(result);
+    }
+}
